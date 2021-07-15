68d0d1e65f4b6fd01bcb621814225eea3c6c1967
Snowfall99 <445117445@qq.com>
2021-07-15 14:37:55 +0800

    111-114

--- solution114.cpp
+++ solution114.cpp
@@ -1,56 +1,34 @@
-// 前序遍历，利用栈
-#include <stack>
-#include <iostream>
-using namespace std;
-// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if (!root) return;
+        if (root == nullptr)
+            return;
 
-        stack<TreeNode*> stk;
-        stk.push(root);
-        TreeNode* cur;
-        TreeNode* prev= NULL;
-        while (!stk.empty()) {
-            cur = stk.top();
-            stk.pop();
-            if (prev) {
-                prev->right = cur;
-                prev->left = NULL;
+        TreeNode* curr = root;
+        while (curr != nullptr) {
+            if (curr->left != nullptr) {
+                TreeNode* next = curr->left;
+                TreeNode* pre = next;
+                while (pre->right != nullptr) {
+                    pre = pre->right;
+                }
+                pre->right = curr->right;
+                curr->right = next;
+                curr->left = nullptr;  
             }
-            if (cur->right) {
-                stk.push(cur->right);
-            }
-            if (cur->left) {
-                stk.push(cur->left);
-            }
-            prev = cur;
+            curr = curr->right;
         }
+
     }
 };
-
-int main() {
-    TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->left->left = new TreeNode(3);
-    root->left->right = new TreeNode(4);
-    root->right = new TreeNode(5);
-    root->right->right = new TreeNode(6);
-    Solution s;
-    s.flatten(root);
-    while(root != NULL) {
-        cout<<root->val<<"\t";
-        root = root->right;
-    }
-    return 0;
-}
